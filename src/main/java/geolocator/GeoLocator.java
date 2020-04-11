e9416da3d8c7ba70c45b802308dd851663ff2aff
Abhishek Agrawal <abhishekagrawal9696@gmail.com>
2020-04-11 20:07:03 +0200

    Added log4j and slf4j

--- src/main/java/geolocator/GeoLocator.java
+++ src/main/java/geolocator/GeoLocator.java
@@ -1,14 +1,12 @@
 package geolocator;
 
 import java.net.URL;
-
 import java.io.IOException;
-
 import com.google.gson.Gson;
-
 import com.google.common.net.UrlEscapers;
-
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Class for obtaining geolocation information about an IP address or host
@@ -18,9 +16,15 @@ import org.apache.commons.io.IOUtils;
 public class GeoLocator {
 
     /**
+     * Creates a logger Object for logging using log4j and sfl4j.
+     */
+    private static Logger logger = LoggerFactory.getLogger(GeoLocator.class);
+
+    /**
      * URI of the geolocation service.
      */
     public static final String GEOLOCATOR_SERVICE_URI = "http://ip-api.com/json/";
+
 
     private static Gson GSON = new Gson();
 
@@ -36,6 +40,7 @@ public class GeoLocator {
      * @throws IOException if any I/O error occurs
      */
     public GeoLocation getGeoLocation() throws IOException {
+        logger.debug("Get GeoLocation");
         return getGeoLocation(null);
     }
 
@@ -52,20 +57,27 @@ public class GeoLocator {
         URL url;
         if (ipAddrOrHost != null) {
             ipAddrOrHost = UrlEscapers.urlPathSegmentEscaper().escape(ipAddrOrHost);
+            logger.debug("IP Address or Host is " + ipAddrOrHost);
             url = new URL(GEOLOCATOR_SERVICE_URI + ipAddrOrHost);
+            logger.info("URL: " + url);
         } else {
             url = new URL(GEOLOCATOR_SERVICE_URI);
+            logger.info("URL: " + url);
         }
         String s = IOUtils.toString(url, "UTF-8");
+        logger.debug("Converting URL to UTF-8 string");
         return GSON.fromJson(s, GeoLocation.class);
     }
 
-    //CHECKSTYLE:OFF
+    // CHECKSTYLE:OFF
     public static void main(String[] args) throws IOException {
+
         try {
             String arg = args.length > 0 ? args[0] : null;
+            logger.debug("Checks Command Line argument");
             System.out.println(new GeoLocator().getGeoLocation(arg));
         } catch (IOException e) {
+            logger.error("IOException occurred:" + e.getMessage());
             System.err.println(e.getMessage());
         }
     }
